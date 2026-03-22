67982c10ff3ed26924cf0170fe2f43274cbd45dc
bbarski <biuro@adbito.pl>
2026-03-22 21:38:19 +0100

    Update map.php

--- components/map.php
+++ components/map.php
@@ -5,10 +5,7 @@ $data = get_data_for_template('benefits');
 	<div class="benefits-container">
 		<h2 class="section-title">Gdzie mnie znajdziesz</h2>
 		<div class="map-container">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.4127754932133!2d21.239162299999986!3d52.34479840000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ec5004fab8441%3A0xc526de902e932c52!2sAdBito%20Us%C5%82ugi%20informatyczne%2C%20serwis%20komputerowy%2C%20Odzyskiwanie%20danych!5e0!3m2!1spl!2spl!4v1768430392325!5m2!1spl!2spl" 
-					width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" 
-					referrerpolicy="no-referrer-when-downgrade">
-		    </iframe>
+//map iframe 
 		</div>	
     </div>
 </section>
