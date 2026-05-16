eac707b420c83f11592be7297ed676452f44234d
bbarski <biuro@adbito.pl>
2026-05-16 07:55:57 +0000

    add blog css for solid and evenement color themes

--- functions.php
+++ functions.php
@@ -9,18 +9,11 @@ function child_starter_theme_enqueue_scripts() {
 }
 
 add_action('wp_head', 'custom_meta_tags', 1);
-function custom_meta_tags() {
-    ?>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#3b3b2e" id="theme-color-meta">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">	
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&display=swap" rel="stylesheet">    
-<?php } ?>
-<?php
+function custom_meta_tags() 
+	{
+        get_template_part('components/elements/meta_tags');
+} 
+
 
 function get_data_for_template($template_name) {
     global $customizer_sections_config;
